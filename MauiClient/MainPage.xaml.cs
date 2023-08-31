70db302794b2dff58e19964a1ff2b4f58c445b77
Netcode-Hub <Rastinga1995@hotmail.com>
2023-08-31 04:13:58 +0000

    Add project files.

---
+++ MauiClient/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+
+namespace MauiClient
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var uploadFile = await MediaPicker.PickPhotoAsync();
+
+            // check for null
+            if (uploadFile is null) return;
+
+            // Read the file
+            var httpContent = new MultipartFormDataContent();
+            httpContent.Add(new StreamContent(await uploadFile.OpenReadAsync()), "file", uploadFile.FileName);
+
+            // Create instance of HttpClient
+            var httpClient = new HttpClient();
+#if ANDROID
+            var result = await httpClient.PostAsync("https://172.22.48.1:7172/api/UlpoadFile", httpContent);
+#elif WINDOWS 
+            var result = await httpClient.PostAsync("https://localhost:7172/api/UlpoadFile", httpContent);
+            var response = await result.Content.ReadAsStringAsync();
+            await Shell.Current.DisplayAlert("Response From Server", response, "Ok");
+        }
+    }
+
+}
---
+++ Server/Controllers/UlpoadFileController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UlpoadFileController : ControllerBase
+    {
+        private readonly ILogger<UlpoadFileController> logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public UlpoadFileController(ILogger<UlpoadFileController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            this.logger = logger;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                // Store the content.
+                var httpContent = HttpContext.Request;
+
+                //check for null
+                if (httpContent is null)
+                    return BadRequest();
+
+                // check if the context contains multiple file.
+                if (httpContent.Form.Files.Count > 0)
+                {
+                    // loop through
+                    foreach (var file in httpContent.Form.Files)
+                    {
+                        // get file path 
+                        var filePath = Path.Combine(webHostEnvironment.ContentRootPath, "ImagesUploadFolder");
+
+                        // check if director exist; if NO then create.
+                        if (!Directory.Exists(filePath))
+                            Directory.CreateDirectory(filePath);
+
+                        //copy the file to the folder
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await file.CopyToAsync(memoryStream);
+                            System.IO.File.WriteAllBytes(Path.Combine(filePath, file.FileName), memoryStream.ToArray());
+                        }
+                    }
+                    return Ok(httpContent.Form.Files.Count.ToString() + " file(s) uploaded");
+                }
+                return BadRequest("No file selected");
+
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                return new StatusCodeResult(500);
+            }
+           
+
+
+        }
+    }
+}
